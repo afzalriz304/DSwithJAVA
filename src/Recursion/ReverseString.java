77245211217bf5403630da468dbe4e399b5ecdf2
Afzal Rizvi <afzalriz304@gmail.com>
2022-08-19 08:10:26 +0530

    "Reverse a String"

--- src/Recursion/ReverseString.java
+++ src/Recursion/ReverseString.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class ReverseString {
 
-    public static String reverse(String str) {
+    private static String reverse(String str) {
         if (str.isEmpty()) {
             return "";
         } else {
@@ -10,7 +10,22 @@ public class ReverseString {
         }
     }
 
+    private static char[] reverse(char[] s, int i, int j) {
+        if (s == null)
+            return null;
+        if (s.length == 1)
+            return s;
+        if (i == j || i>j)
+            return s;
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        return reverse(s, i+1, j-1);
+    }
+
     public static void main(String[] args) {
         System.out.println(reverse("Hello"));
+        char[] s = new char[]{'h', 'e', 'l', 'l', 'o'};
+        System.out.println(reverse(s, 0, s.length - 1));
     }
 }
